f142202803c78dc1fbf92877cd395ff61836574e
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2022-04-18 21:04:34 +0100

    Fixed Episode 42

--- episode-42/src/SoldierForm.svelte
+++ episode-42/src/SoldierForm.svelte
@@ -6,8 +6,8 @@ import { kiaRegular, wiaToKia } from "./stores"
 
 <form>
   <CommonSliders />
-  <Slider label="Percentage of KIA that are regular troops" min={50} max={100} value={$kiaRegular} format={(v) => `${v}%`} />
-  <Slider label="Wounded to Killed Ratio" min={100} max={1000} value={$wiaToKia} format={(v) => `${v/100.0}x`} />
+  <Slider label="Percentage of KIA that are regular troops" min={50} max={100} bind:value={$kiaRegular} format={(v) => `${v}%`} />
+  <Slider label="Wounded to Killed Ratio" min={100} max={1000} bind:value={$wiaToKia} format={(v) => `${v/100.0}x`} />
 </form>
 
 <style>
