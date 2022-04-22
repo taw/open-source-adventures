435077154ce0cdd35f73655365039b423ad492a9
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2022-04-22 20:48:47 +0100

    Episode 43 - use relative paths

--- episode-43/vite.config.js
+++ episode-43/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite"
+import solidPlugin from "vite-plugin-solid"
 
 export default defineConfig({
+  base: "./",
   plugins: [solidPlugin()],
   build: {
-    target: 'esnext',
+    target: "esnext",
     polyfillDynamicImport: false,
   },
-});
+})
