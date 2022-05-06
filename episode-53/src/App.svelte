4ad64243f1761f02c653763c73261295c398311f
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2022-05-06 17:52:44 +0100

    Episode 53

---
+++ episode-53/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import data from "./data.json"
+</script>
+
+<h1>Weapons Data</h1>
+
+<table>
+	{#each data as row}
+		<tr>
+			<td>{row.name}</td>
+		</tr>
+	{/each}
+</table>
+
+<style>
+:global(body) {
+	margin: 0;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+</style>
