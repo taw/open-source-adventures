4d25363100a5f18ae5a3f1f6e220e0526365ed95
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2022-05-06 18:34:28 +0100

    Got rid of TABs

--- episode-35/src/App.svelte
+++ episode-35/src/App.svelte
@@ -5,26 +5,26 @@ import TankLosses from "./TankLosses.svelte"
 let parseRow = ({date,tank}) => ({date: new Date(date), tank: +tank})
 
 let loadData = async () => {
-	let url = "./russia_losses_equipment.csv"
-	let data = await d3.csv(url, parseRow)
-	data.unshift({date: new Date("2022-02-24"), tank: 0})
-	return data
+  let url = "./russia_losses_equipment.csv"
+  let data = await d3.csv(url, parseRow)
+  data.unshift({date: new Date("2022-02-24"), tank: 0})
+  return data
 }
 
 let dataPromise = loadData()
 </script>
 
 {#await dataPromise then data}
-	<TankLosses {data} />
+  <TankLosses {data} />
 {/await}
 
 <style>
 :global(body) {
-	margin: 0;
-	min-height: 100vh;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+  margin: 0;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
