d69caf80130675645cca6a635afe7b99981a5964
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2022-07-03 11:14:50 +0100

    Minesweeper Puzzle solver

---
+++ episode-65/minesweeper.cr
@@ -0,0 +1,77 @@
+#!/usr/bin/env crystal
+
+require "z3"
+
+class MinesweeperSolver
+  @data : Array(Array(Nil | Int32))
+  @ysize : Int32
+  @xsize : Int32
+
+  def initialize(path)
+    @data = File.read_lines(path).map{|l| l.chars.map{|c| c.ascii_number? ? c.to_i : nil}}
+    @ysize = @data.size
+    @xsize = @data[0].size
+    @solver = Z3::Solver.new
+    @vars = {} of Tuple(Int32, Int32) => Z3::BoolExpr
+  end
+
+  def each_coord
+    @ysize.times do |y|
+      @xsize.times do |x|
+        yield(x, y)
+      end
+    end
+  end
+
+  def neighbourhood(x, y)
+    [
+      @vars[{x-1, y-1}]?,
+      @vars[{x-1, y}]?,
+      @vars[{x-1, y+1}]?,
+      @vars[{x, y-1}]?,
+      @vars[{x, y+1}]?,
+      @vars[{x+1, y-1}]?,
+      @vars[{x+1, y}]?,
+      @vars[{x+1, y+1}]?,
+    ].compact
+  end
+
+  def neighbourhood_count(x, y)
+    neighbourhood(x, y).map{|v| v.ite(1, 0)}.reduce{|a,b| a+b}
+  end
+
+  def call
+    each_coord do |x, y|
+      @vars[{x, y}] = Z3.bool("#{x},#{y}")
+    end
+    each_coord do |x, y|
+      c = @data[y][x]
+      if c
+        @solver.assert neighbourhood_count(x, y) == c
+        @solver.assert ~@vars[{x,y}]
+      end
+    end
+    if @solver.check
+      model = @solver.model
+      @ysize.times do |y|
+        @xsize.times do |x|
+          v = (model[@vars[{x, y}]].to_s == "true")
+          c = @data[y][x]
+          if c
+            print c
+          elsif v
+            print "*"
+          else
+            print " "
+          end
+        end
+        print "\n"
+      end
+    else
+      puts "No solution"
+    end
+  end
+end
+
+solver = MinesweeperSolver.new(ARGV[0])
+solver.call
